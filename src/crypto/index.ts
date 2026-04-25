e54abde80673bbab8b5486e0ce81b99cacfcf08e
Ovidiu Eftimie <eovidiu@gmail.com>
2026-04-25 23:37:00 +0300

    fix(F-004): remove loadKey from public surface; amend spec to encryption-primitives-only scope (M4+D1)

--- src/crypto/index.ts
+++ src/crypto/index.ts
@@ -1,4 +1,4 @@
 export { encryptToken } from "./encrypt";
 export type { EncryptResult } from "./encrypt";
 export { decryptToken } from "./decrypt";
-export { IntegrityError, importKey as loadKey } from "./key";
+export { IntegrityError } from "./key";
